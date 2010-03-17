df22230d9ce82479d501b3b8eae871eca1f7847c
Raine Virta <raine.virta@nodeta.fi>
2010-03-17 14:54:27 +0200

    archive.sh: Ignore README.rdoc

--- archive.sh
+++ archive.sh
@@ -1,4 +1,4 @@
 # Creates a ZIP archive (.webapp) out of contents of the directory excluding
 # some unnecessary files
 
-zip flowdock.webapp * -x flowdock.webapp archive.sh
+zip flowdock.webapp * -x flowdock.webapp archive.sh README.rdoc
